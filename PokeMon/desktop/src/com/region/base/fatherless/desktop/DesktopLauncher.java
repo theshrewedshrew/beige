b85b4e1a624448497d535f528e6d7e54ade35520
Taylor Hudson <theshrewedshrew@gmail.com>
2017-01-21 10:47:13 -0600

    Push - 1/21/2017
    10:47 am

---
+++ PokeMon/desktop/src/com/region/base/fatherless/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.region.base.fatherless.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.region.base.fatherless.UnknownTrainer;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new UnknownTrainer(), config);
+	}
+}
